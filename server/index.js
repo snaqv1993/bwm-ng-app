9818f91376e884cf229cc958b58ea5de2313965a
snaqvi1993 <snaqvi1993@gmail.com>
2019-04-16 16:22:31 +1000

    Added pre deploy setup

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const config = require('./config/dev');
+const config = require('./config');
 const rental = require('./models/rental');
 const FakeDb = require('./fake-db');
+const path = require('path');
 const rentalRoutes = require('./routes/rentals'),
-	  userRoutes = require('./routes/users'),
-	  bookingRoutes = require('./routes/bookings');
+userRoutes = require('./routes/users'),
+bookingRoutes = require('./routes/bookings');
 
 mongoose.connect(config.DB_URI, { useNewUrlParser: true, sslValidate: true }).then(()=>{
-	const fakeDb = new FakeDb();
+	if(process.env.NODE_ENV !== 'production'){
+		const fakeDb = new FakeDb();
 	// console.log(config.DB_URI);
 	// fakeDb.seedDb();
 	// console.log(config.DB_URI);
+}
+
 }).catch((err) => {
 	console.log(err.message);
 });
@@ -22,6 +26,18 @@ app.use(bodyParser.json());
 app.use('/api/v1/rentals', rentalRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/bookings', bookingRoutes);
+
+if(process.env.NODE_ENV === 'production') {
+	const appPath = path.join(__dirname, '..', 'dist/bwm-ng-app');
+
+	app.use(express.static(appPath));
+
+	app.get('*', function(req, res) {
+		res.sendFile(path.resolve(appPath, 'index.html'));
+	})
+}
+
+
 
 //const PORT = process.env.PORT || 3001;
 
