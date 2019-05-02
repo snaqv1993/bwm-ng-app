ed8a825fc00f0bb8e2bfcb3d64e176b64dbf17bf
snaqvi1993 <snaqvi1993@gmail.com>
2019-05-02 10:32:33 +1000

    Added image edit functionality

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,5 +1,7 @@
 module.exports = {
 DB_URI : process.env.DB_URI,
-SECRET : process.env.SECRET
+SECRET : process.env.SECRET,
+AWS_ACCESS_KEY_ID : 'AKIAI53LVJAC7DBXGGDA',
+AWS_SECRET_ACCESS_KEY : '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'
 }
 
